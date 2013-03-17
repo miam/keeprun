8a587a5f26765cd8418ab7474e6f4cb2e14e8594
Richard Refi <miamano@gmail.com>
2013-03-18 00:02:37 +0100

    autossh functionality added

---
+++ autossh.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -x
+
+HOST="sage"
+
+AUTOSSH_PATH="/usr/bin/ssh"
+export AUTOSSH_PATH
+
+AUTOSSH_PIDFILE="/home/miam/bin/autossh.pid"
+export AUTOSSH_PIDFILE
+
+PIDFILE="$AUTOSSH_PIDFILE"
+AUTOSSH_CMD="/usr/bin/autossh"
+
+call_autossh ()
+{
+	"$AUTOSSH_CMD" -M 22023 -N -R 22022:localhost:22 -f "$HOST"	
+}
+
+self_check ()
+{
+	if [ -f "$PIDFILE" ]; then
+		PID=`cat "$PIDFILE"`
+		kill -0 $PID
+		if [ $? -eq 1 ]; then
+			return 1
+		else
+			return 0
+		fi
+	else
+		return 1
+	fi
+}
+
+self_check
+if [ $? -eq 1 ]; then
+	call_autossh
+fi
